39d56e6a8e7c2598616e9b16c8c071025342536b
chenshaotong <13144061737@163.com>
2021-08-07 14:04:55 +0800

    保存

--- src/main/java/cn/chenshaotong/mapper/UserMapper.java
+++ src/main/java/cn/chenshaotong/mapper/UserMapper.java
@@ -2,12 +2,21 @@ package cn.chenshaotong.mapper;
 
 import cn.chenshaotong.dto.UserDto;
 import cn.chenshaotong.entity.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class UserMapper {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    static {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.registerModule(new JavaTimeModule());
+    }
     public User mapToUser(UserDto userDto){
         return objectMapper.convertValue(userDto,User.class);
     }
--- src/test/java/cn/chenshaotong/controller/UserControllerTest.java
+++ src/test/java/cn/chenshaotong/controller/UserControllerTest.java
@@ -12,6 +12,11 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @PropertySource({"classpath:application.yml"})
@@ -36,14 +41,31 @@ class UserControllerTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    void test3() {
-        UserDto userDto = UserDto.builder().name("CST").build();
-        User user = UserMapper.mapToUser(userDto);
-        User save = userRepository.save(user);
-        System.out.println(save.toString());
-    }
+//    @Test
+//    void test3() {
+//        UserDto userDto = UserDto.builder().name("CST").build();
+//        User user = UserMapper.mapToUser(userDto);
+//        User save = userRepository.save(user);
+//        System.out.println(save.toString());
+//    }
 
+    @Test
+    void test4() {
+        List<User> all = userRepository.findAll();
+        all.forEach(e->{
+            UserDto userDto  = UserMapper.mapToUserDto(e);
+            System.out.println(userDto.toString());
+        });
+
+        List<UserDto> cst = all.stream().map(e -> {
+            return UserMapper.mapToUserDto(e);
+        }).filter(e -> e.getName().equals("CST")).collect(Collectors.toList());
+
+        int size = cst.size();
+        System.out.println(size);
+
+
+    }
 
 
 
