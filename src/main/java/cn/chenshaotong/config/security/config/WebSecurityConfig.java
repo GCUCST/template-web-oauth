946904decd02e76c62497f117fa0bed4311a37f6
chenshaotong <13144061737@163.com>
2022-01-21 21:29:36 +0800

    保存前后端分离使用oauth模板

--- src/main/java/cn/chenshaotong/config/security/config/WebSecurityConfig.java
+++ src/main/java/cn/chenshaotong/config/security/config/WebSecurityConfig.java
@@ -1,42 +1,37 @@
 package cn.chenshaotong.config.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+// @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired public UserDetailsService springDataUserDetailsService;
+  //  @Autowired public UserDetailsService springDataUserDetailsService;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    super.configure(web);
-  }
+  //
+  //  @Override
+  //  public void configure(WebSecurity web) throws Exception {
+  //    super.configure(web);
+  //  }
 
   // 认证用户的来源
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    //        auth.inMemoryAuthentication()
-    //                .withUser("cst")
-    //                .password("{noop}123")//{noop}代表原文
-    //                .roles("ADMIN");
-    auth.userDetailsService(springDataUserDetailsService).passwordEncoder(passwordEncoder());
-  }
+  //  @Override
+  //  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  //    //        auth.inMemoryAuthentication()
+  //    //                .withUser("cst")
+  //    //                .password("{noop}123")//{noop}代表原文
+  //    //                .roles("ADMIN");
+  //    auth.userDetailsService(springDataUserDetailsService).passwordEncoder(passwordEncoder());
+  //  }
 
   // 相关信息，拦截规则
   //    @Override
